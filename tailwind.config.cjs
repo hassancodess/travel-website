0f173ca04091faa44a654067e6927661f7c7b954
hassancodess <dev.hassancodes@gmail.com>
2022-08-22 11:08:56 +0500

    Add Fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Sora', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
