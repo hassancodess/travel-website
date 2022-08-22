9c590d6c9446e9aa640170456ed3c95cfcbc9a9f
hassancodess <dev.hassancodes@gmail.com>
2022-08-22 15:09:10 +0500

    fix Hero & Navbar position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import beach from '../assets/beachVid.mp4'
+import { FaSearch } from 'react-icons/fa'
+
 function Hero() {
   return (
     <>
-      <section className='h-screen w-screen relative -z-10'>
+      {/* Video Background */}
+      <section className='relative -z-10'>
         <video
           src={beach}
           autoPlay
           loop
           muted
-          className='w-full h-full object-cover'
+          className='h-screen w-screen absolute  object-cover'
         ></video>
-        <div className='container mx-auto max-w-7xl'></div>
+        {/* Overlay */}
+        <div className='absolute top-0  w-screen h-screen bg-gradient-to-t from-gray-700  to-black opacity-50 '></div>
+        {/* Content */}
+        <div className='h-screen w-screen top-0 absolute z-50'>
+          <div className='container mx-auto max-w-7xl h-full w-full flex flex-col space-y-8 items-center justify-center'>
+            <h1 className='text-2xl xl:text-5xl font-bold text-white text-center'>
+              First Class Travel
+            </h1>
+            <h1 className='text-2xl font-bold xl:text-5xl text-white tex-center'>
+              Top 1% Locations Worldwide
+            </h1>
+            <div className='flex items-center bg-white px-4 py-2 w-full max-w-xs md:max-w-md xl:max-w-3xl rounded-lg'>
+              <input
+                type='text '
+                className='w-full py-2 px-3 bg-transparent outline-none'
+                placeholder='Enter email'
+              />
+              <div className='bg-blue-400 text-white p-2 rounded-lg'>
+                <FaSearch size={25} />
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,67 +26,69 @@ function Navbar() {
   return (
     <>
       {/* Nav Container */}
-      <nav className='p-4'>
+      <nav className='absolute w-full p-4 z-50'>
         {/* Main Container */}
         <div className='container mx-auto max-w-7xl'>
           {/* Flex Container */}
-          <div className='flex justify-between items-center relative'>
-            {/* Logo */}
-            <h1 className='uppercase text-2xl font-bold'>Beaches.</h1>
-            {/* Menu */}
-            <div className='hidden md:flex space-x-10'>
-              <a href='#'>Home</a>
-              <a href='#'>Destinations</a>
-              <a href='#'>Travel</a>
-              <a href='#'>Views</a>
-              <a href='#'>Book</a>
-            </div>
-            {/* Icons */}
-            <div className='hidden md:flex space-x-6'>
-              <FaSearch size={18} />
-              <FaUser size={18} />
-            </div>
-            {/* Mobile Hamburger */}
-            <div className='md:hidden' onClick={toggleNav}>
-              {nav ? <IoClose size={25} /> : <FiMenu size={25} />}
-            </div>
-            {/* Mobile Menu */}
-            <div
-              className={`absolute top-14 left-0 w-full rounded-xl text-white text-center bg-black bg-opacity-30 px-10 py-5  ${
-                nav ? '' : 'hidden'
-              }`}
-            >
-              <div className='flex flex-col space-y-8'>
-                {/* Menu */}
-                <div className='flex flex-col space-y-4'>
-                  <a href='#' className='border-b pb-2'>
-                    Home
-                  </a>
-                  <a href='#' className='border-b pb-2'>
-                    Destinations
-                  </a>
-                  <a href='#' className='border-b pb-2'>
-                    Travel
-                  </a>
-                  <a href='#' className='border-b pb-2'>
-                    Views
-                  </a>
-                  <a href='#' className='border-b pb-2'>
-                    Book
-                  </a>
-                </div>
-                {/* Buttons */}
-                <div className='flex flex-col space-y-2'>
-                  <button className={styles.gradientBtn}>Search</button>
-                  <button className={styles.gradientBtn}>Account</button>
-                </div>
-                {/* Social Icons */}
-                <div className='flex justify-between text-gray-800'>
-                  <IoLogoFacebook size={25} className={styles.socialIcon} />
-                  <IoLogoTwitter size={25} className={styles.socialIcon} />
-                  <IoLogoInstagram size={25} className={styles.socialIcon} />
-                  <IoLogoPinterest size={25} className={styles.socialIcon} />
-                  <IoLogoYoutube size={25} className={styles.socialIcon} />
+          <div className='relative'>
+            <div className='flex justify-between items-center text-white'>
+              {/* Logo */}
+              <h1 className='uppercase text-2xl font-bold '>Beaches.</h1>
+              {/* Menu */}
+              <div className='hidden md:flex space-x-10 '>
+                <a href='#'>Home</a>
+                <a href='#'>Destinations</a>
+                <a href='#'>Travel</a>
+                <a href='#'>Views</a>
+                <a href='#'>Book</a>
+              </div>
+              {/* Icons */}
+              <div className='hidden md:flex space-x-6 '>
+                <FaSearch size={18} />
+                <FaUser size={18} />
+              </div>
+              {/* Mobile Hamburger */}
+              <div className='md:hidden' onClick={toggleNav}>
+                {nav ? <IoClose size={25} /> : <FiMenu size={25} />}
+              </div>
+              {/* Mobile Menu */}
+              <div
+                className={`absolute top-14 left-0 w-full rounded-xl text-white text-center bg-black px-10 py-5  ${
+                  nav ? '' : 'hidden'
+                }`}
+              >
+                <div className='flex flex-col space-y-8'>
+                  {/* Menu */}
+                  <div className='flex flex-col space-y-4'>
+                    <a href='#' className='border-b pb-2'>
+                      Home
+                    </a>
+                    <a href='#' className='border-b pb-2'>
+                      Destinations
+                    </a>
+                    <a href='#' className='border-b pb-2'>
+                      Travel
+                    </a>
+                    <a href='#' className='border-b pb-2'>
+                      Views
+                    </a>
+                    <a href='#' className='border-b pb-2'>
+                      Book
+                    </a>
+                  </div>
+                  {/* Buttons */}
+                  <div className='flex flex-col space-y-2'>
+                    <button className={styles.gradientBtn}>Search</button>
+                    <button className={styles.gradientBtn}>Account</button>
+                  </div>
+                  {/* Social Icons */}
+                  <div className='flex justify-between text-blue-300'>
+                    <IoLogoFacebook size={25} className={styles.socialIcon} />
+                    <IoLogoTwitter size={25} className={styles.socialIcon} />
+                    <IoLogoInstagram size={25} className={styles.socialIcon} />
+                    <IoLogoPinterest size={25} className={styles.socialIcon} />
+                    <IoLogoYoutube size={25} className={styles.socialIcon} />
+                  </div>
                 </div>
               </div>
             </div>
